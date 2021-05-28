6b5103df050388fe7eb85fe1d3c5991f8f8dffd9
Laxmi Gurung <72887242+laxmigurung@users.noreply.github.com>
2021-05-28 17:30:22 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.body.style.background = "#f3f3f3 url('backimage.img') no-repeat right top";
+  }
+  function animationbg() {
+    document.body.style.background = "#f3f3f3 url('anime.jpg') no-repeat right top";
+  }
+  function clubhouse() {
+    document.body.style.background = "#f3f3f3 url('clubhouse.jpg') no-repeat right top";
+  }
