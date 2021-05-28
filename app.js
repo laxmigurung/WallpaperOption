3572e3f590aa4a14f2715870f40fa8e32eb4897e
Laxmi Gurung <72887242+laxmigurung@users.noreply.github.com>
2021-05-28 15:38:25 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    document.body.style.background = "url('backimage.img') no-repeat right top";
-  }
-  function animationbg() {
-    document.body.style.background = "url('anime.jpg') no-repeat right top";
-  }
-  function clubhouse() {
-    document.body.style.background = "url('clubhouse.jpg') no-repeat right top";
-  }
